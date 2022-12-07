70b2afe208b00c31d646e68e83696b4764eaa661
bbacks <wnsrud6839@gmail.com>
2022-12-07 18:48:17 +0900

    [pjk] 파일 정리중

---
+++ homepage/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>웨이브온</title>
+	<link rel="stylesheet" href="./styles/main.css?v=4"/>
+	<script charset="UTF-8" class="daum_roughmap_loader_script" src="//dmaps.daum.net/map_js_init/roughmapLoader.js"></script>
+</head>
+<body>
+	<div class="wrap" id="div-top">
+		HELLO
+	<div>
+</body>
+</html>
